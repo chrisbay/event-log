650da19e57b1dbe990af60829e516f5262638bc9
Chris Bay <chris@chrisbay.net>
2018-03-14 14:27:03 -0500

    Adds Spring Boot starter

---
+++ src/main/java/net/chrisbay/eventlog/EventLogApplication.java
@@ -0,0 +1,12 @@
+package net.chrisbay.eventlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventLogApplication.class, args);
+	}
+}
