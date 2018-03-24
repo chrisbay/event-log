251ba6b54b080f5077d87b7e43e6d79eeae6c508
Chris Bay <chris@chrisbay.net>
2018-03-24 12:02:07 -0500

    Adds equals and hashCode to AbstractEntity

--- src/main/java/net/chrisbay/eventlog/models/AbstractEntity.java
+++ src/main/java/net/chrisbay/eventlog/models/AbstractEntity.java
@@ -3,6 +3,7 @@ package net.chrisbay.eventlog.models;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import java.util.Objects;
 
 /**
  * Created by Chris Bay
@@ -18,4 +19,17 @@ public abstract class AbstractEntity {
         return this.uid;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity that = (AbstractEntity) o;
+        return uid == that.uid;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(uid);
+    }
 }
